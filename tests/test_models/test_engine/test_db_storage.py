7ddee2689f6b45d22277b5ceb626919864696eb3
Rwinkah <rwinosa@gmail.com>
2023-03-22 23:37:24 +0100

    fix

---
+++ tests/test_models/test_engine/test_db_storage.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" tests for db storage """
+import unittest
+from models.base_model import BaseModel
+import MySQLdb
+
+
+@unittest.skipif(os.getenv('HBNB_TYPE_STORAGE') == 'db',
+                 'filestorage test not supported')
+class test_db_storage(unittest.Testcase):
+    """ class to test db storage engine """
+
+    def setup(self):
+        """ create test env """
+        user = os.getenv('HBNB_MYSQL_USER')
+        pwd = os.getenv('HBNB_MYSQL_PWD')
+        host = os.getenv('HBNB_MYSQL_HOST')
+        db = os.get_env('HBNB_MYSQL_DB')
+
+        self.connt = msq.connect(user=user, host=host, db=db, passwd=pwd)
+        self.curs = connt.cursor()
+        self.curs.execute("CREATE DATABASE IF NOT EXISTS test_db")
+        self.curs.execute("CREATE TABLE IF NOT EXISTS state")
+
+    def teardowm(self):
+        """ cleanup after test """
+        self.curs.execute("DROP DATABASE IF EXISTS test_db")
+        self.curs.close()
+        self.connt.close()
