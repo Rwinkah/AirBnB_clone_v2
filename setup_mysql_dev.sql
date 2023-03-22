f69144c8618d2bc0f473a32f2899d9263e2885d9
anita-jamgbadi <jamgbanita@gmail.com>
2023-03-22 21:28:16 +0000

    Task 3

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -8,3 +8,5 @@ GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
+GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
