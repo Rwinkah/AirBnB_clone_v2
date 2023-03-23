097c8358e58172fef51e53b2269a734933d426e6
Rwinkah <rwinosa@gmail.com>
2023-03-23 01:42:43 +0100

    fix

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -3,7 +3,7 @@
 -- Grant user all privileges on this db
 -- Grant user SELECT privilege on this db
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test`_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
