d9962c2811239e609fb25d5ce68ffdb591e2d47d
Rwinkah <rwinosa@gmail.com>
2023-03-23 01:41:46 +0100

    fix: syntax error

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -2,9 +2,9 @@
 -- Next creat new user in local host and set password to hbnb_dev_pwd
 -- Grant user all privileges on this db
 -- Grant user SELECT privilege on this db
-CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test`_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
